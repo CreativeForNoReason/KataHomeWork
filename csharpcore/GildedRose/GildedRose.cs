ceed09a8eafbc6cffde05aaf12f53b993ba01f08
CreativeForNoReason <domasnorkunas@gmail.com>
2023-02-14 22:59:35 +0200

    Seperating logic for different items

--- csharpcore/GildedRose/GildedRose.cs
+++ csharpcore/GildedRose/GildedRose.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace GildedRoseKata
 {
@@ -14,75 +15,87 @@ namespace GildedRoseKata
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                switch (Items[i].Name)
                 {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-                        }
-                    }
+                    case "Backstage passes to a TAFKAL80ETC concert":
+                        SolveForBackstage(Items[i]);
+                        break;
+                    case "Aged Brie":
+                        SolveForAgedBrie(Items[i]);
+                        break;
+                    case "Sulfuras, Hand of Ragnaros":
+                        continue;
+                    case "Conjured Mana Cake":
+                        SolveForConjured(Items[i]);
+                        break;
+                    default:
+                        SolveForOthers(Items[i]);
+                        break;
                 }
 
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                Items[i].SellIn = Items[i].SellIn - 1;
+            }
+        }
+
+        private void SolveForConjured(Item item)
+        {
+            if (item.Quality > 0)
+            {
+                item.Quality = item.Quality - 2;
+            }
+
+            if (item.SellIn <= 0)
+            {
+                item.Quality = Math.Max(item.Quality - 2, 0);
+            }
+        }
+
+        private void SolveForOthers(Item item)
+        {
+            if (item.Quality > 0)
+            {
+                item.Quality = item.Quality - 1;
+            }
+
+            if (item.SellIn <= 0)
+            {
+                item.Quality = Math.Max(item.Quality - 1, 0);
+            }
+        }
+
+        private void SolveForAgedBrie(Item item)
+        {
+            if (item.Quality < 50)
+            {
+                item.Quality = item.Quality + 1;
+            }
+
+            if (item.SellIn <= 0)
+            {
+                item.Quality = Math.Min(item.Quality + 1, 50);
+            }
+        }
+
+        private void SolveForBackstage(Item item)
+        {
+            if (item.Quality < 50)
+            {
+                item.Quality = item.Quality + 1;
+
+                if (item.SellIn < 11)
                 {
-                    Items[i].SellIn = Items[i].SellIn - 1;
+                    item.Quality = Math.Min(item.Quality + 1, 50);
                 }
 
-                if (Items[i].SellIn < 0)
+                if (item.SellIn < 6)
                 {
-                    if (Items[i].Name != "Aged Brie")
-                    {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].Quality > 0)
-                            {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
-                    }
+                    item.Quality = Math.Min(item.Quality + 1, 50);
                 }
+            }
+
+            if (item.SellIn == 0)
+            {
+                item.Quality = 0;
             }
         }
     }
